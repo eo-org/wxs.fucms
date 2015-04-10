0705f023d3820dbfbd9238aebcb8fedff6f7696e
kingavin <craftgavin@gmail.com>
2015-04-10 17:08:11 +0800

    aa

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -13,38 +13,7 @@ class IndexController extends AbstractActionController
     
     public function getUserCodeAction()
     {
-    	$sm = $this->getServiceLocator();
     	
-    	$userAuth = $sm->get('User\Service\SessionAuth');
-    	
-    	if($userAuth->isLogin()) {
-    		
-    	}
-    	
-    	$code = $this->params()->fromQuery('code');
-    	
-    	$ch = curl_init();
-    	curl_setopt($ch, CURLOPT_URL, 'http://wx.fucmsweb.com/api/component-access-token');
-    	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    	$output = curl_exec($ch);
-    	curl_close($ch);
-    	
-    	$tokenObj = json_decode($output);
-    	
-    	$componentAccessToken = $tokenObj->componentAccessToken;
-    	
-    	
-    	$url = "https://api.weixin.qq.com/sns/oauth2/component/access_token?appid=wx536a9272e58807e7&code=".$code."&grant_type=authorization_code&component_appid=wx2ce4babba45b702d&component_access_token=".$componentAccessToken;
-    	
-    	$ch = curl_init();
-    	curl_setopt($ch, CURLOPT_URL, $url);
-    	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-// //    	curl_setopt($ch, CURLOPT_POST, 1);
-//     	//curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-    	$output = curl_exec($ch);
-    	curl_close($ch);
-    	
-    	print_r($output);
     	
     }
 }
--- module/User/Module.php
+++ module/User/Module.php
@@ -34,13 +34,52 @@ class Module
     	$sm = $e->getApplication()->getServiceManager();
     	
     	$rm = $e->getRouteMatch();
-    	$matchedRouteName = $rm->getMatchedRouteName();
+    	$router =  $e->getRouter();  
     	
     	$userAuth = $sm->get('User\Service\SessionAuth');
     	
     	if(!$userAuth->isLogin()) {
-    		header("Location: https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx536a9272e58807e7&redirect_uri=http%3A%2F%2Fwxs.fucmsweb.com%2Fget-user-code&response_type=code&scope=snsapi_base&state=gavin&component_appid=wx2ce4babba45b702d#wechat_redirect");
-    		exit(0);
+    		
+    		$query = $e->getRequest()->getQuery();
+    		
+    		if(isset($query['state']) && $query['state'] == 'access-openid') {
+    			if(isset($query['code'])) {
+    				
+    				$requestUri = $router->getRequestUri();
+    				
+    				$code = $query['code'];
+    				
+    				$ch = curl_init();
+    				curl_setopt($ch, CURLOPT_URL, 'http://wx.fucmsweb.com/api/component-access-token');
+    				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    				$output = curl_exec($ch);
+    				curl_close($ch);
+    				 
+    				$tokenObj = json_decode($output);
+    				 
+    				$componentAccessToken = $tokenObj->componentAccessToken;
+    				 
+    				 
+    				$url = "https://api.weixin.qq.com/sns/oauth2/component/access_token?appid=wx536a9272e58807e7&code=".$code."&grant_type=authorization_code&component_appid=wx2ce4babba45b702d&component_access_token=".$componentAccessToken;
+    				 
+    				$ch = curl_init();
+    				curl_setopt($ch, CURLOPT_URL, $url);
+    				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    				$output = curl_exec($ch);
+    				curl_close($ch);
+    				
+    				 
+    				$openIdObj = json_decode($output);
+    				$openId = $openIdObj->openid;
+    				 
+    				$userAuth->setOpenId($openId);
+    			} else {
+    				throw new \Exception('not allowed');
+    			}
+    		} else {
+    			header("Location: https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx536a9272e58807e7&redirect_uri=http%3A%2F%2Fwxs.fucmsweb.com%2Fuser&response_type=code&scope=snsapi_base&state=access-openid&component_appid=wx2ce4babba45b702d#wechat_redirect");
+    			exit(0);
+    		}
     	}
     }
 }
